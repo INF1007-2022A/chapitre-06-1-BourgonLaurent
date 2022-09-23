69d3bd787727f2d77a2ffc900d7fdb54fa9c24ef
Laurent Bourgon <laurent.bourgon@outlook.com>
2022-09-23 16:02:13 -0400

    [ex1] implemented order

--- exercice.py
+++ exercice.py
@@ -2,12 +2,28 @@
 # -*- coding: utf-8 -*-
 
 
-def order(values: list = None) -> list:
+def order(values: list[str | float] = None) -> list:
     if values is None:
-        # TODO: demander les valeurs ici
-        pass
+        values = []
+        while len(values) < 10:
+            value = input("Entrez une valeur (type: int | float | str): ")
+            try:
+                # Try to convert to float,
+                # will work if int or float, positive or negative (-)
+                # Raising ValueError if it is a str
+                value = float(value)
+                if value.is_integer():
+                    # Reassign to casted int
+                    value = int(value)
+            except ValueError:  # it is a string, don't reassign it
+                pass
 
-    return []
+            values.append(value)
+
+    # Convert floats to strings to compare them
+    values.sort(key=str)
+
+    return values
 
 
 def anagrams(words: list = None) -> bool:
@@ -46,19 +62,23 @@ def print_recipe(ingredients) -> None:
 
 def main() -> None:
     print(f"On essaie d'ordonner les valeurs...")
-    order()
+    print(order())
 
     print(f"On vérifie les anagrammes...")
-    anagrams()
+    print(anagrams())
 
     my_list = [3, 3, 5, 6, 1, 1]
     print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
 
     grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
     best_student = best_grades(grades)
-    print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
+    print(
+        f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}"
+    )
 
-    sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    sentence = (
+        "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    )
     frequence(sentence)
 
     print("On enregistre les recettes...")
@@ -68,5 +88,5 @@ def main() -> None:
     print_recipe(recipes)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
